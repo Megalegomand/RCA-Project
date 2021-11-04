d14e8bca89d2c08134f3a6a16c695020420736e2
Simon C. Dockweiler <scdockweiler@gmail.com>
2021-11-04 13:45:48 +0100

    Tilføjede online fjern

--- AssigmentFiles/gazebo_server.sh
+++ AssigmentFiles/gazebo_server.sh
@@ -2,9 +2,10 @@
 
 # Call: ./gazebo_server world.world
 
+
 DIR="$( cd "$( dirname "$1" )" >/dev/null && pwd )"
-echo $DIR
-export GAZEBO_MODEL_PATH=$DIR/models/:$GAZEBO_MODEL_PATH 
+export GAZEBO_MODEL_DATABASE_URI=""
+export GAZEBO_MODEL_PATH=$DIR/models/
 
 export GAZEBO_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/gazebo-11/plugins/:$GAZEBO_PLUGIN_PATH
 export GAZEBO_PLUGIN_PATH=$DIR/marble_contact_plugin/build:$GAZEBO_PLUGIN_PATH
