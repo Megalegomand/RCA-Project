cb4070981fa072bffb52d91696e3fed8419e174e
Rasmus Gjerlund <rasg@sdu.dk>
2021-11-07 21:21:17 +0100

    tilføjet fil

---
+++ src/ai/src/reinforcement.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+
+int main(int argc, char** argv) {
+
+	cout << "hej" << endl;
+
+}
