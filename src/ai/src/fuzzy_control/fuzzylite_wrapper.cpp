3b31dc33e9f4f3d5f8fb42d42dda6fbc04e5f6d3
Rasmus <rasmc19@student.sdu.dk>
2021-11-10 15:10:16 +0100

    Obstacles works

--- src/ai/src/fuzzy_control/fuzzylite_wrapper.cpp
+++ src/ai/src/fuzzy_control/fuzzylite_wrapper.cpp
@@ -27,6 +27,8 @@ void FuzzyLiteWrapper::init_engine()
     engine->setName("TargetSteeringModule");
     engine->setDescription("");
 
+    // Input variables for Obstacles Avoidance ------------------------
+
     InputVariable *obs_angle = new InputVariable;
     obs_angle->setName("obs_angle");
     obs_angle->setDescription("");
@@ -34,23 +36,11 @@ void FuzzyLiteWrapper::init_engine()
     obs_angle->setRange(-LIDAR_RANGE, LIDAR_RANGE);
     obs_angle->setLockValueInRange(false);
     obs_angle->addTerm(new Ramp("far_left", -M_PI, -LIDAR_RANGE));
-    obs_angle->addTerm(new Triangle("left", -0.200,-M_PI/8, -M_PI));
+    obs_angle->addTerm(new Triangle("left", -M_PI,-M_PI/8,-0.200 ));
     obs_angle->addTerm(new Triangle("center", -0.200, 0.000, 0.200));
     obs_angle->addTerm(new Triangle("right", 0.200, M_PI/8, M_PI));
     obs_angle->addTerm(new Ramp("far_right", M_PI, LIDAR_RANGE));
     engine->addInputVariable(obs_angle);
-
-    // InputVariable *target_angle = new InputVariable;
-    // target_angle->setName("target_angle");
-    // target_angle->setDescription("");
-    // target_angle->setEnabled(true);
-    // target_angle->setRange(-LIDAR_RANGE, LIDAR_RANGE);
-    // target_angle->setLockValueInRange(false);
-    // target_angle->setLockPreviousValue(false);
-    // target_angle->addTerm(new Ramp("left", 0.000, -LIDAR_RANGE));
-    // target_angle->addTerm(new Triangle("center", -0.500, 0.000, 0.500));
-    // target_angle->addTerm(new Ramp("right", 0.000, LIDAR_RANGE));
-    // engine->addInputVariable(target_angle);
 
     InputVariable *obs_distance = new InputVariable;
     obs_distance->setName("obs_distance");
@@ -58,10 +48,38 @@ void FuzzyLiteWrapper::init_engine()
     obs_distance->setEnabled(true);
     obs_distance->setRange(0.000,10.000 );
     obs_distance->setLockValueInRange(false);
-    obs_distance->addTerm(new Ramp("close", 2.000, 0.000));
-    obs_distance->addTerm(new Triangle("medium", 2.000, 6.000, 8.000));
-    obs_distance->addTerm(new Ramp("far", 8.000, 10.000));
+    obs_distance->addTerm(new Ramp("close", 1.500, 0.000));
+    obs_distance->addTerm(new Triangle("medium", 1.500, 3.000, 5.000));
+    obs_distance->addTerm(new Ramp("far", 5.000, 10.000));
     engine->addInputVariable(obs_distance);
+
+    // Input variables for Target Steering --------------------------
+
+    // InputVariable *angleTarget = new InputVariable;
+    // angleTarget->setName("angleTarget");
+    // angleTarget->setDescription("");
+    // angleTarget->setEnabled(true);
+    // angleTarget->setRange(-LIDAR_RANGE, LIDAR_RANGE);
+    // angleTarget->setLockValueInRange(false);
+    // angleTarget->setLockPreviousValue(false);
+    // angleTarget->addTerm(new Ramp("left", 0.000, -LIDAR_RANGE));
+    // angleTarget->addTerm(new Triangle("center", -0.500, 0.000, 0.500));
+    // angleTarget->addTerm(new Ramp("right", 0.000, LIDAR_RANGE));
+    // engine->addInputVariable(angleTarget);
+
+    // InputVariable *angleSteering = new InputVariable;
+    // angleSteering->setName("angleSteering");
+    // angleSteering->setDescription("");
+    // angleSteering->setEnabled(true);
+    // angleSteering->setRange(-LIDAR_RANGE, LIDAR_RANGE);
+    // angleSteering->setLockValueInRange(false);
+    // angleSteering->setLockPreviousValue(false);
+    // angleSteering->addTerm(new Ramp("left", 0.000, -LIDAR_RANGE));
+    // angleSteering->addTerm(new Triangle("center", -0.500, 0.000, 0.500));
+    // angleSteering->addTerm(new Ramp("right", 0.000, LIDAR_RANGE));
+    // engine->addInputVariable(angleSteering);
+
+    // Output variables -------------------------------------------
 
     OutputVariable *turn_speed = new OutputVariable;
     turn_speed->setName("turn_speed");
@@ -73,11 +91,11 @@ void FuzzyLiteWrapper::init_engine()
     turn_speed->setDefuzzifier(new Centroid(100));
     turn_speed->setDefaultValue(fl::nan);
     turn_speed->setLockPreviousValue(false);
-    turn_speed->addTerm(new Ramp("fast_left", -0.30, -0.40));
-    turn_speed->addTerm(new Triangle("left", -0.10,-0.20, -0.30));
+    turn_speed->addTerm(new Ramp("fast_left", -0.25, -0.40));
+    turn_speed->addTerm(new Triangle("left",-0.25,-0.20,-0.10 ));
     turn_speed->addTerm(new Triangle("center", -0.10, 0.00, 0.10));
-    turn_speed->addTerm(new Triangle("right", 0.10, 0.20, 0.30));
-    turn_speed->addTerm(new Ramp("fast_right", 0.30, 0.40));
+    turn_speed->addTerm(new Triangle("right", 0.10, 0.20, 0.25));
+    turn_speed->addTerm(new Ramp("fast_right", 0.25, 0.40));
     engine->addOutputVariable(turn_speed);
 
     OutputVariable *velocity = new OutputVariable;
@@ -86,20 +104,20 @@ void FuzzyLiteWrapper::init_engine()
     velocity->setEnabled(true);
     velocity->setRange(0, 1.40);
     velocity->setLockValueInRange(false);
-    velocity->setAggregation(new Maximum);
+    velocity->setAggregation(new AlgebraicProduct); // before maximum
     velocity->setDefuzzifier(new Centroid(100));
     velocity->setDefaultValue(fl::nan);
     velocity->setLockPreviousValue(false);
-    velocity->addTerm(new Ramp("slow", 0.450, 0.000));
-    velocity->addTerm(new Triangle("medium", 0.450,0.685, 0.90));
-    velocity->addTerm(new Ramp("fast", 0.90, 1.40));
+    velocity->addTerm(new Ramp("slow", 0.200, 0.000));
+    velocity->addTerm(new Triangle("medium", 0.20, 0.400, 0.800));
+    velocity->addTerm(new Ramp("fast", 0.600, 1.30));
     engine->addOutputVariable(velocity);
 
     RuleBlock *mamdani = new RuleBlock;
     mamdani->setName("mamdani");
     mamdani->setDescription("");
     mamdani->setEnabled(true);
-    mamdani->setConjunction(new Minimum);
+    mamdani->setConjunction(new AlgebraicProduct); // before minimum
     mamdani->setDisjunction(fl::null);
     mamdani->setImplication(new AlgebraicProduct);
     mamdani->setActivation(new General);
@@ -187,6 +205,10 @@ void FuzzyLiteWrapper::update(float *vel, float *turn_speed,
     // Update input values
     obs_distance_iv->setValue(obs_distance);
     obs_angle_iv->setValue(obs_angle);
+    //target_distance_iv->setValue();
+    //target_angle_iv->setValue();
+
+    //Point(20,3); // Goal
 
     engine->process();
 
