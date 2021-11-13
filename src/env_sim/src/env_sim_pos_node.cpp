b0cd228a8ece91dcb9de596814d147abda26388a
Simon C. Dockweiler <scdockweiler@gmail.com>
2021-11-13 16:30:05 +0100

    Trying to make target work

--- src/env_sim/src/env_sim_pos_node.cpp
+++ src/env_sim/src/env_sim_pos_node.cpp
@@ -1,7 +1,7 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "gazebo_msgs/GetModelState.h"
-#include "robot/"
+#include "robot/Position.h"
 
 bool position_callback(robot::Position::Request& req, robot::Position::Response& res)
 {
@@ -9,16 +9,19 @@ bool position_callback(robot::Position::Request& req, robot::Position::Response&
     model_state.request.model_name = "";
     if (ros::service::call("/gazebo/get_model_state", model_state))
     {
-        ROS_INFO("%s", model_state.response.pose);
+        res.pose = model_state.response.pose;
+        return true;
     }
-    return true;
+    return false;
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "position_node");
+    ros::NodeHandle n;
+    ros::ServiceServer service = n.advertiseService("robot/position", position_callback);
 
-    ros::spinOnce();
+    ros::spin();
 
     return 0;
 }
