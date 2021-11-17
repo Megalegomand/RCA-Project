aba6f298bdd2b92dca974ac6eccce06d14542338
Simon C. Dockweiler <scdockweiler@gmail.com>
2021-11-17 15:54:14 +0100

    Target mere

--- src/robot/src/robot_target_node.cpp
+++ src/robot/src/robot_target_node.cpp
@@ -1,10 +1,27 @@
 #include "ros/ros.h"
+#include "robot/GetPoint.h"
+#include "geometry_msgs/Point.h"
+#include <mutex>
+
+std::mutex target_mutex;
+geometry_msgs::Point target;
+
+bool get_point_callback(robot::GetPoint::Request& req, robot::GetPoint::Response& res)
+{
+    target_mutex.lock();
+    res.point = target;
+    target_mutex.unlock();
+    return true;
+}
+
 
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "robot_target_node");
 
     ros::NodeHandle n;
+
+
 
     return 0;
 }
