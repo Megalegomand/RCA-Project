666541318b0e14147359ed7a0e6a2fa780fd39ea
Simon C. Dockweiler <scdockweiler@gmail.com>
2021-11-27 17:29:18 +0100

    Vision test setup

--- src/vision/src/test/blur_test.cpp
+++ src/vision/src/test/blur_test.cpp
@@ -1,15 +1,21 @@
 #include "ros/ros.h"
 #include <gtest/gtest.h>
+#include "sensor_msgs/Image.h"
+
+using namespace ros;
+using namespace testing;
 
 TEST(TestSuite, testCase2)
 {
+    NodeHandle n;
+    ImageConstPtr img_raw ros::topic::waitForMessage<sensor_msgs::Image>("/robot/image_raw", n, DURATION_MAX);
+
     EXPECT_TRUE(true);
 }
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
-  testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "blur_test");
-  ros::NodeHandle nh;
+  InitGoogleTest(&argc, argv);
+  init(argc, argv, "blur_test");
   return RUN_ALL_TESTS();
 }
