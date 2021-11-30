8b1c3af8a02cc1c74f46e9c375a28e9468610929
Rasmus <rasmc19@student.sdu.dk>
2021-11-30 13:34:05 +0100

    added class

---
+++ src/reinforcement_learning/src/Qlearn.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "state.h"
+#include "envoriment.h"
+
+using namespace std;
+using namespace cv;
+
+
+class Qlearn
+{
+	
+private:
+
+	//Siblings /children /connected states
+	vector<State*> Qtable;
+
+public:
+
+Qlearn();
+Qlearn(int x_coordinate_, int y_coordinate_, int reward_);
+~Qlearn();
+
+};
